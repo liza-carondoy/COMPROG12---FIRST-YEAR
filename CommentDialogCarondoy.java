b434b6890a1b73accdfdbfaa588aaf489a4852ce
Liza <113318781+liza-carondoy@users.noreply.github.com>
2024-02-15 16:50:32 +0800

    Create CommentDialogCarondoy.java

---
+++ CommentDialogCarondoy.java
@@ -0,0 +1,23 @@
+/*Name: Liza N. Carondoy
+ * Course: BSIT
+ * Date: 2/4/2024
+ */
+
+ import javax.swing.JOptionPane;
+
+public class CommentDialogCarondoy {
+
+    public static void main(String[] args) {
+
+        //Single line comment
+
+    //"Program comments are nonexecuting statements you add to a file for the purpose of documentation."
+    
+        //Multi line comment
+
+    /*"Program comments are nonexecuting statements you
+        add to a file for the purpose of documentation."*/
+
+        JOptionPane.showMessageDialog(null, "Program comments are nonexecuting statements you add to a file for the purpose of documentation.");
+    }
+}
