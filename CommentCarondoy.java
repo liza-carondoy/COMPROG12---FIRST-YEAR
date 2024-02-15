4158fdf94ab504f54ad0647ccf9de24361c8ab6b
Liza <113318781+liza-carondoy@users.noreply.github.com>
2024-02-15 16:50:49 +0800

    Create CommentCarondoy.java

---
+++ CommentCarondoy.java
@@ -0,0 +1,24 @@
+/* Name: Liza N.Carondoy
+ * Course: BSIT
+ * Date: 2/4/2024
+ */
+
+public class CommentCarondoy {
+
+    public static void main(String[] args) {
+
+        //Single line comment
+
+    //"Program comments are nonexecuting statements you add to a file for the purpose of documentation."
+    
+        //Multi line comment
+
+    /*"Program comments are nonexecuting statements you
+        add to a file for the purpose of documentation."*/
+
+        //Javadoc comment
+
+     /**"Program comments are nonexecuting statements you
+        add to a file for the purpose of documentation."*/
+    }
+}
