9879a853004d66110c9f953ccf6095eae810e183
Liza <113318781+liza-carondoy@users.noreply.github.com>
2024-03-17 19:03:43 +0800

    StudentCarondoy.java

--- StudentCarondoy.java
+++ StudentCarondoy.java
@@ -1,22 +1,39 @@
-/* Name: Liza Carondoy
-   Midterm Activity #3 */
-
+/* NAME: Liza Carondoy
+   DATE: February 15, 2023
+   Activity: 3 (MIDTERM) */
+   
 public class StudentCarondoy {
+    private int idNumber;
+    private int creditHoursEarned;
+    private int pointsEarned;
+    private double gradePointAverage;
     private String name;
     private int numScores;
     private int highestScore;
     private int lowestScore;
     private double averageScore;
 
-    // Constructor
-    public StudentCarondoy(String name) {
-        this.name = name;
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public void setCreditHoursEarned(int creditHoursEarned) {
+        this.creditHoursEarned = creditHoursEarned;
+    }
+
+    public void setPointsEarned(int pointsEarned) {
+        this.pointsEarned = pointsEarned;
+    }
+
+    public void calculateGPA() {
+        if (creditHoursEarned != 0) {
+            gradePointAverage = (double) pointsEarned / creditHoursEarned;
+        } else {
+            gradePointAverage = 0.0; // Handle division by zero
+        }
     }
 
     // Accessor methods (getters and setters)
-    public String getName() {
-        return name;
-    }
 
     public void setNumScores(int numScores) {
         this.numScores = numScores;
@@ -48,5 +65,8 @@ public class StudentCarondoy {
 
     public double getAverageScore() {
         return averageScore;
+    }
+    public void displayStudentInfo() {
+        System.out.println("Grade Point Average: " + gradePointAverage);
     }
 }
